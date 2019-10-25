aa3fe16114523b6218116bc086b16cce08fc3431
Kalana Shalitha <kalana.s@villvay.com>
2019-10-25 11:04:01 +0530

    added bundle js

--- src/main/java/demo/DemoApplication.java
+++ src/main/java/demo/DemoApplication.java
@@ -2,11 +2,12 @@ package demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @SpringBootApplication
 public class DemoApplication {
     private StudentRepository studentRepository;
@@ -23,7 +24,7 @@ public class DemoApplication {
     public String home() {
         System.out.println("root");
         //System.exit(1);
-        return "Hello Docker World";
+        return "index.html";
     }
 
     @RequestMapping("/save")
